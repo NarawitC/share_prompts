9a4df6819036fe08895b87afd1f967c8846d202c
NarawitC <narawit.developer@gmail.com>
2023-07-08 11:54:17 +0700

    wait

---
+++ components/Nav.jsx
@@ -0,0 +1,10 @@
+'use client';
+import Link from 'next/link';
+import Image from 'next/image';
+import { useState, useEffect } from 'react';
+import { sigIn, signOut, useSession, getProviders } from 'next-auth/react';
+
+const Nav = () => {
+  return <div>Nav</div>;
+};
+export default Nav;
